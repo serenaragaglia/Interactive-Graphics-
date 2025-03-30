91d833c8b284d7c1324ca76945c529f135b4a157
serenaragaglia <ragaglia.1941007@studenti.uniroma1.it>
2025-03-30 17:14:44 +0200

    Update project2.js

--- homework_2/project2.js
+++ homework_2/project2.js
@@ -3,6 +3,20 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
+
+	let rotation_angle = (Math.PI/180)*rotation;
+
+	let scale_matrix = [scale, 0, 0, 
+					    0, scale, 0,
+					    0, 0, 1	];
+	let rotation_matrix = [Math.cos(rotation_angle), -Math.sin(rotation_angle), 0,
+					       Math.sin(rotation_angle), Math.cos(rotation_angle), 0,
+					       0,0,1];
+	let translation_matrix = [1, 0, positionX,
+						      0, 1, positionY,
+						      0, 0, 1];
+	
+
 	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
 }
 
