54379ec8f068d833a1476e89812756a089dfb7a6
serenaragaglia <ragaglia.1941007@studenti.uniroma1.it>
2025-04-01 15:55:22 +0200

    final project 2

--- homework_2/project2.js
+++ homework_2/project2.js
@@ -18,31 +18,42 @@ function GetTransform( positionX, positionY, rotation, scale )
 	let rotation_matrix = [cos, sin,  0,
 					       -sin, cos, 0,
 					       0,    0,   1];
-	let translation_matrix = [1, 0, positionX,
-						      0, 1, positionY,
-						      0, 0, 1];
-	//Call to ApplyTrabsform to have the transformations in the requested order
-	transformation = ApplyTransform(ApplyTransform(scale_matrix, rotation_matrix), translation_matrix) //rotation and scaling can be interexchanged, the problem is with the translation
-	return transformation;
+	let translation_matrix = [1, 0, 0,
+						      0, 1, 0,
+						      positionX, positionY, 1];
+
+	//Call to ApplyTransform to have the transformations in the requested order
+	tempTransformation = ApplyTransform(scale_matrix, rotation_matrix);
+	return ApplyTransform(tempTransformation, translation_matrix);
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
 // The arguments are transformation matrices in the same format.
-// The returned transformation first applies trans1 and then trans2.
-function ApplyTransform( trans1, trans2 ) //in this function we have to perform a multiplication between two matrices, keeping in mind that it's necessary to follow a precise order
+// The returned transformation first applies t2 and then t1.
+function ApplyTransform( t1, t2 ) //in this function we have to perform a multiplication between two matrices, keeping in mind that it's necessary to follow a precise order
 {
-   return [
+  /*the product should be like
+        t2[0]*t1[0] + t2[3]*t1[1] + t2[6]*t1[2],
+		t2[1]*t1[0] + t2[4]*t1[1] + t2[7]*t1[2],
+		t2[2]*t1[0] + t2[5]*t1[1] + t2[8]*t1[2],
+		
+        t2[0]*t1[3] + t2[3]*t1[4] + t2[6]*t1[5],
+		t2[1]*t1[3] + t2[4]*t1[4] + t2[7]*t1[5],
+		t2[2]*t1[3] + t2[5]*t1[4] + t2[8]*t1[5],
 
-        trans1[0]*trans2[0] + trans1[3]*trans2[1] + trans1[6]*trans2[2],
-        trans1[0]*trans2[3] + trans1[3]*trans2[4] + trans1[6]*trans2[5],
-        trans1[0]*trans2[6] + trans1[3]*trans2[7] + trans1[6]*trans2[8],
+        t2[0]*t1[6] + t2[3]*t1[7] + t2[6]*t1[8],             
+        t2[1]*t1[6] + t2[4]*t1[7] + t2[7]*t1[8],
+        t2[2]*t1[6] + t2[5]*t1[7] + t2[8]*t1[8] */
 
-        trans1[1]*trans2[0] + trans1[4]*trans2[1] + trans1[7]*trans2[2],
-        trans1[1]*trans2[3] + trans1[4]*trans2[4] + trans1[7]*trans2[5],
-        trans1[1]*trans2[6] + trans1[4]*trans2[7] + trans1[7]*trans2[8],
+	let i,j,k; let result = new Array(9);
+	result.fill(0) ;
+	for(i=0; i < 3; i++){
+		for(j=0; j < 3; j++){
+			for(k=0; k < 3; k++){
+				result[i*3 + j] += t2[k*3 + j]*t1[i*3 + k] ;
+			}
+		}
+	}
 
-        trans1[2]*trans2[0] + trans1[5]*trans2[1] + trans1[8]*trans2[2],
-        trans1[2]*trans2[3] + trans1[5]*trans2[4] + trans1[8]*trans2[5],
-        trans1[2]*trans2[6] + trans1[5]*trans2[7] + trans1[8]*trans2[8]
-    ];
+	return result;
 }
